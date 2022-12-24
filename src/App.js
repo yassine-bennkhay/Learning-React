c090059a268244616e7897fc391e3a35d2da8ced
Yassine <benkhayyassine8@gmail.com>
2022-12-24 05:58:02 -0800

    hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Header from "./components/Header";
-
+import FeedbackData from "./data/FeedbackData";
+import FeadbackList from "./components/FeedbackList";
+import Card from "./components/shared/Card";
 function App() {
+  const [feedback,setFeedback]=useState(FeedbackData)
   return (
     <>
     <Header/>
+    
     <div className="container">
+     <FeadbackList feedback={feedback}/>
+    
     </div>
+   
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 function Header({text ,bgColor,textColor }) {
   const headerStyles = {
     backgroundColor: bgColor,
