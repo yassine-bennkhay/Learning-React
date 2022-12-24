3cf30ec1410c3fe46f623b9222fa10649490c894
Yassine <benkhayyassine8@gmail.com>
2022-12-24 08:58:44 -0800

    remove review

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import FeadbackList from "./components/FeedbackList";
 import Card from "./components/shared/Card";
+import FeedbackStats from "./components/FeedbackStats";
 function App() {
-  const [feedback,setFeedback]=useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <>
-    <Header/>
-    
-    <div className="container">
-     <FeadbackList feedback={feedback}/>
-    
-    </div>
-   
+      <Header />
+
+      <div className="container">
+        <FeedbackStats feedback={feedback}/>
+        <FeadbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
     </>
   );
 }
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import FeedbackData from "../data/FeedbackData";
 import FeedbackItem from "./FeedbackItem";
-
-function FeedbackList({ feedback }) {
-  if(!feedback||feedback.length===0){
-    return <p>No Feedback Yet.</p>
+import PropTypes from "prop-types";
+function FeedbackList({ feedback ,handleDelete}) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet.</p>;
   }
-  return <div className="feedback-list">
-   {feedback.map((item)=>(
-   <FeedbackItem key={item.id} item={item}/>
-   ))}
-  </div>;
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+      ))}
+    </div>
+  );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(PropTypes.shape({
+    id:PropTypes.number.isRequired,
+    text:PropTypes.string.isRequired,
+    rating:PropTypes.number.isRequired,
+    
+  })),
+};
 
 export default FeedbackList;
